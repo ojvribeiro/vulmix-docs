7f4a4d475114e8112076ca90e70979de1feaebad
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-12 14:41:55 -0300

    feat(tailwind): added Vulmix brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./app.vue', './pages', './components'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vx: {
+          'brand-green': '#3DDA98',
+          'brand-pink': '#F3A1F0',
+          'grey-400': '#232323',
+        },
+      },
+    },
   },
   plugins: [],
 }
