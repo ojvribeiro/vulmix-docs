818304d5392af5c6862311ae534906abbd613365
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-12 17:15:10 -0300

    feat: created navbar logo

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div
-    class="fixed top-0 left-0 w-full py-3 bg-vx-grey-400/80 backdrop-blur-md"
+    class="fixed top-0 left-0 w-full h-16 bg-vx-grey-400/80 backdrop-blur-md"
   >
-    <Container>
-      Navbar
+    <Container class="h-full">
+      <div class="flex items-center h-full py-3">
+        <NavbarLogo />
+      </div>
     </Container>
   </div>
 </template>
