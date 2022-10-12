8d426cdff2b672b514245648818a304882f5c158
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-12 19:01:27 -0300

    feat: added navbar links

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -3,9 +3,51 @@
     class="fixed top-0 left-0 w-full h-16 bg-vx-grey-400/80 backdrop-blur-md"
   >
     <Container class="h-full">
-      <div class="flex items-center h-full py-3">
+      <div class="flex items-center justify-between h-full py-3">
         <NavbarLogo />
+
+        <div class="flex items-center gap-6">
+          <div>
+            <ul class="nav-menu flex gap-4">
+              <li>
+                <NuxtLink to="#">Guide</NuxtLink>
+              </li>
+
+              <li>
+                <NuxtLink to="#">FAQs</NuxtLink>
+              </li>
+
+              <li>
+                <NuxtLink to="#">Showcases</NuxtLink>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/ojvribeiro/vulmix"
+                  target="_blank"
+                  rel="nofollow noreferrer noopener"
+                >
+                  <Icon name="fa:github" class="text-2xl" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </Container>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  ul {
+    li {
+      a {
+        @apply text-white hover:text-white/70;
+      }
+    }
+  }
+</style>
