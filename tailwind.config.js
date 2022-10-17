544e4579d4d5c7abdc3c9f126d9ca94a02f1620d
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-16 21:56:19 -0300

    feat(tailwind): added more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,23 @@ module.exports = {
     extend: {
       colors: {
         vx: {
-          'brand-green': '#3DDA98',
-          'brand-pink': '#F3A1F0',
-
+          'grey-200': '#808080',
           'grey-300': '#4d4d4d',
           'grey-400': '#232323',
+
+          'green-100': '#1DBA78',
+          'green-200': '#199461',
+
+          brand: {
+            green: '#3DDA98',
+            pink: '#F3A1F0',
+          },
+
+          alert: {
+            warning: 'rgb(255 251 0 / 10%)',
+            note: 'rgb(0 137 255 / 16%)',
+            danger: 'rgb(255 0 0 / 16%)',
+          },
         },
       },
     },
