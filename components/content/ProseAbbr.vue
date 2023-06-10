69d3090d96f23cec7ff5db61f71589e8df1363e6
Victor Ribeiro <devjvribeiro@gmail.com>
2023-06-10 17:18:56 -0300

    chore: created abbr prose component

---
+++ components/content/ProseAbbr.vue
@@ -0,0 +1,15 @@
+<template>
+  <abbr :title="props.title"><slot /></abbr>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    title: string
+  }>()
+</script>
+
+<style scoped>
+  abbr {
+    @apply text-vx-brand-pink cursor-help border-b border-vx-brand-pink no-underline border-dashed;
+  }
+</style>
